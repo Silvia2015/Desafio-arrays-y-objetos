6123de4e65a37a73ab4f73028b9030839d1ffa20
LucianoCesc <cisternas.luciano@gmail.com>
2022-10-04 22:57:36 -0300

    Creacion de Arrays datos agenda

---
+++ assets/js/index.js
@@ -0,0 +1,151 @@
+let radiologia = [
+    {hora: '11:00',
+    especialista: 'Ignacio Shulz',
+    paciente: {
+        nombre: 'Francisca Rojas',
+        rut: '9878782-1',
+        prevision: 'Fonasa'
+        }
+    },
+    {hora: '11:30',
+    especialista: 'Federico Subercaseaux',
+    paciente: {
+        nombre: 'Pamela Estrada',
+        rut: '15345241-3',
+        prevision: 'Isapre'
+        }
+    },
+
+    {hora: '15:00',
+    especialista: 'Fernando Wurthz',
+    paciente: {
+        nombre: 'Armando Luna',
+        rut: '16445345-9',
+        prevision: 'Isapre'
+        }
+    },
+    {hora: '15:30',
+    especialista: 'Ana Godoy',
+    paciente: {
+        nombre: 'Manuel Godoy',
+        rut: '17666419-0',
+        prevision: 'Fonasa'
+        }
+    },
+    {hora: '16:00',
+    especialista: 'Patricia Suazo',
+    paciente: {
+        nombre: 'Ramon Ulloa',
+        rut: '14989389-k',
+        prevision: 'Fonasa'
+        }
+    }
+]
+let traumatologia= [
+    {hora: '8:00',
+    especialista: 'Maria Altuzarra',
+    paciente: {
+        nombre: 'Paula SAnchez',
+        rut: '15554774-5',
+        prevision: 'Fonasa'
+        }
+    },
+    {hora: '10:00',
+    especialista: 'Raul Araya',
+    paciente: {
+        nombre: 'Angelica Navas',
+        rut: '15444147-9',
+        prevision: 'Isapre'
+        }
+    },
+    {hora: '10:30',
+    especialista: 'Maria Arriagada',
+    paciente: {
+        nombre: 'Ana Klapp',
+        rut: '17879423-9',
+        prevision: 'Isapre'
+        }
+    },
+    {hora: '11:00',
+    especialista: 'Alejandro Badilla',
+    paciente: {
+        nombre: 'Felipe Mardones',
+        rut: '1547423-6',
+        prevision: 'Isapre'
+        }
+    },
+    {hora: '11:30',
+    especialista: 'Cecilia Budnik',
+    paciente: {
+        nombre: 'Diego Marre',
+        rut: '16554741-k',
+        prevision: 'Fonasa'
+        }
+    },
+    {hora: '12:00',
+    especialista: 'Arturo Cabagnaro',
+    paciente: {
+        nombre: 'Cecilia Mendez',
+        rut: '9747535-8',
+        prevision: 'Isapre'
+        }
+    },
+    {hora: '12:30',
+    especialista: 'Andres Kanacri',
+    paciente: {
+        nombre: 'Marcial Suazo',
+        rut: '11254785-5',
+        prevision: 'Isapre'
+        }
+    }
+]
+let dental=[
+    {hora: '8:30',
+    especialista: 'Andrea Zuñiga',
+    paciente: {
+        nombre: 'Marcela Retamal',
+        rut: '11123425-6',
+        prevision: 'Isapre'
+        }
+    },
+    {hora: '11:00',
+    especialista: 'Maria Zañartu',
+    paciente: {
+        nombre: 'Angel Muñoz',
+        rut: '9878789-2',
+        prevision: 'Isapre'
+        }
+    },
+    {hora: '11:30',
+    especialista: 'Scarlet Witting',
+    paciente: {
+        nombre: 'Mario Kkast',
+        rut: '7998789-5',
+        prevision: 'Fonasa'
+        }
+    },
+    {hora: '13:00',
+    especialista: 'Francisco Vonteuber',
+    paciente: {
+        nombre: 'Karim Fernandez',
+        rut: '18887662-k',
+        prevision: 'Fonasa'
+        }
+    },
+    {hora: '13:30',
+    especialista: 'Eduardo Viñuela',
+    paciente: {
+        nombre: 'Hugo Sanchez',
+        rut: '17665461-4',
+        prevision: 'Fonasa'
+        }
+    },
+    {hora: '14:00',
+    especialista: 'Raquel Villaseca',
+    paciente: {
+        nombre: 'Ana Sepulveda',
+        rut: '14441281-0',
+        prevision: 'Isapre'
+        }
+    }
+]
